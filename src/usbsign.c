ac20f2215203e83712d1e9a8b8939ea0f9cc7e24
Nick Parker <nickbp@users.sourceforge.net>
2009-06-03 22:46:14 -0400

    add abstraction to support libusb-0.1 -- untested

---
+++ src/usbsign.c
@@ -0,0 +1,129 @@
+/************************************************************************\
+
+  bbusb - BetaBrite Prism LED Sign Communicator
+  libusb abstraction layer
+  Copyright (C) 2009  Nicholas Parker <nickbp@gmail.com>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+\************************************************************************/
+
+#include "usbsign.h"
+
+int usbsign_init(void) {
+#ifdef NOUSB
+    printf("USB Init\n");
+    return 0;
+#else
+#ifdef OLDUSB
+    usb_init();
+    usb_find_busses();
+    usb_find_devices();
+
+    return 0;
+#else
+    return libusb_init(NULL);
+#endif
+#endif
+}
+
+int usbsign_open(int vendorid, int productid,
+                 int interface, usbsign_handle** dev) {
+#ifdef NOUSB
+    printf("USB Open %X:%X %p:%d\n",vendorid,productid,(void*)dev,interface);
+    return 0;
+#else
+
+#ifdef OLDUSB
+    struct usb_bus* bus;
+    struct usb_device* usbdev;
+    int found = 0;
+
+    for (bus = usb_get_busses(); bus; bus = bus->next) {
+        for (usbdev = bus->devices; usbdev; usbdev = usbdev->next) {
+            if (usbdev->descriptor.idVendor == vendorid &&
+                usbdev->descriptor.idProduct == productid) {
+                found = 1;
+                break;
+            }
+        }
+        if (found > 0) {
+            break;
+        }
+    }
+
+    *dev = usb_open(usbdev);
+#else
+    *dev = libusb_open_device_with_vid_pid(NULL, vendorid, productid);
+#endif
+    if (*dev == NULL) {
+        printerr("Could not find/open USB device with vid=0x%X pid=0x%X. Is the sign plugged in?\n",
+                vendorid, productid);
+        return -1;
+    }
+
+#ifdef OLDUSB
+    int ret = usb_claim_interface(*dev, interface);
+#else
+    int ret = libusb_claim_interface(*dev, interface);
+#endif
+    if (ret < 0) {
+        printerr("Could not claim device (%d)\n", ret);
+        return ret;
+    }
+    return 0;
+
+#endif
+}
+
+void usbsign_close(usbsign_handle* dev, int interface) {
+#ifdef NOUSB
+    printf("USB Close %p:%d\n",dev, interface);
+#else
+#ifdef OLDUSB
+    usb_release_interface(dev, interface);
+    usb_close(dev);
+    dev = NULL;
+#else
+    libusb_release_interface(dev, interface);
+    libusb_close(dev);
+    dev = NULL;
+    libusb_exit(NULL);
+#endif
+#endif
+}
+
+int usbsign_send(usbsign_handle* dev, int endpoint,
+                 char* data, unsigned int size, int* sentcount) {
+#ifdef NOUSB
+    *sentcount = size;
+    printf("USB Send %d bytes of data *%p to device *%p:%d\n",
+           size,data,dev,endpoint);
+    return 0;
+#else
+#ifdef OLDUSB
+    int ret = usb_bulk_write(dev, endpoint, data, size, 1000);
+    if (ret >= 0) {
+        *sentcount = ret;
+        return 0;
+    } else {
+        return ret;
+    }   
+#else
+    return libusb_bulk_transfer(dev, (endpoint | LIBUSB_ENDPOINT_OUT),
+                                (unsigned char*)data, size,
+                                sentcount, 1000);
+#endif
+#endif
+}
---
+++ src/usbsign.h
@@ -0,0 +1,54 @@
+/************************************************************************\
+
+  bbusb - BetaBrite Prism LED Sign Communicator
+  Copyright (C) 2009  Nicholas Parker <nickbp@gmail.com>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+\************************************************************************/
+
+#ifndef __USBSIGN_H__
+#define __USBSIGN_H__
+
+#include "common.h"
+
+#ifdef NOUSB
+
+//stub
+typedef void usbsign_handle;
+
+#else
+#ifdef OLDUSB
+
+//libusb-0.1
+#include <usb.h>
+typedef usb_dev_handle usbsign_handle;
+
+#else
+
+//libusb-1.0
+#include <libusb-1.0/libusb.h>
+typedef libusb_device_handle usbsign_handle;
+
+#endif
+#endif
+
+int usbsign_init(void);
+int usbsign_open(int vendorid, int productid, 
+                 int interface, usbsign_handle** devh);
+void usbsign_close(usbsign_handle* dev, int interface);
+int usbsign_send(usbsign_handle* dev, int endpoint,
+                 char* data, unsigned int size, int* sentcount);
+
+#endif
