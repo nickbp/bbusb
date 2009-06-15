a81ce7dc21074fae21161ba7bd06a7c032f07583
Nick Parker <nickbp@users.sourceforge.net>
2009-06-15 19:43:58 -0400

    add header to bbrss

--- scripts/bbrss.py
+++ scripts/bbrss.py
@@ -1,3 +1,23 @@
+#!/usr/bin/python
+
+'''
+  bbrss.py - Prints a single random article from a specified RSS feed
+  Copyright (C) 2009  Nicholas Parker <nickbp@gmail.com>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 paths = {
     'reuters':'http://feeds.reuters.com/reuters/topNews?format=xml',
     'cnn':'http://rss.cnn.com/rss/cnn_topstories.rss',
